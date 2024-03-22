c5b0721926b5b75aec859599ffdc3daab83738a9
MushfiqZahid <mushfiq.zahid@gmail.com>
2024-03-22 12:06:34 +0600

    product final

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ import authRoutes from "./routes/authRoutes.js";
 
 import categoryRoutes from "./routes/categoryRoutes.js";
 
+import productRoutes from "./routes/productRoutes.js";
+
 import cors from 'cors'; 
 
 
@@ -34,6 +36,7 @@ app.use(morgan('dev'));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 // rest api
 app.get('/', (req, res) => {
