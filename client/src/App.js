08d1d95cea964a0fb6d8c4a23b28bd2f41f3721c
MushfiqZahid <mushfiq.zahid@gmail.com>
2024-03-22 23:47:05 +0600

    Cart frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -46,6 +46,8 @@ import Categories from "./pages/Categories.js";
 
 import CategoryProduct from "./pages/CategoryProduct.js";
 
+import CartPage from "./pages/CartPage";
+
 
 
 
@@ -60,6 +62,7 @@ function App() {
       <Route path="/" element={<HomePage />}/>
       <Route path="/product/:slug" element={<ProductDetails />} />
       <Route path="/categories" element={<Categories />} />
+      <Route path="/cart" element={<CartPage />} />
       <Route path="/category/:slug" element={<CategoryProduct />} />
       <Route path="/search" element={<Search />} />
       <Route path="/dashboard" element={<PrivateRoute />}>
