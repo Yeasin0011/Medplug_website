f19b49c0eed86d9fe8a7eebaa9d8117056c46a03
Yeasin0011 <yeasin.ibne.kadir@g.bracu.ac.bd>
2024-03-29 03:14:39 +0600

    Admin Controller for User Orders

--- client/src/pages/Admin/AdminOrders.js
+++ client/src/pages/Admin/AdminOrders.js
@@ -1,34 +1,59 @@
-import React ,{useState, useEffect} from 'react'
-import Layout from '../../components/Layout/Layout'
-import UserMenu from '../../components/Layout/UserMenu'
+import React, {useState, useEffect} from 'react'
+
 import axios from 'axios'
+
+import AdminMenu from "../../components/Layout/AdminMenu"
+
+import Layout from 'antd/es/layout/layout'
+
+import toast from 'react-hot-toast'
 import { useAuth } from "../../context/auth"
 import moment from "moment"; 
+import { Select } from 'antd'
+const { Option } = Select
 
-const Orders = () => {
-  const [orders, setOrders] = useState([])
-const [auth, setAuth] = useAuth()
-  const getOrders = async() =>{
-    try {
-      const {data} = await axios.get('api/v1/auth/orders');
-      setOrders(data);
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  useEffect(()=> {
-    if(auth?.token) getOrders()
-  }, [auth?.token]); 
 
-  return (
-  <Layout title={'Order History'}>
-    <div className='container-fluid m-3 p-3'>
-        <div className='row'>
-            <div className='col-md-3'>
-                <UserMenu/>
+const AdminOrders = () => {
+    const [status, setStatus] = useState([
+      "Not Process",
+      "Processing",
+      "Shipped",
+      "deliverd",
+      "cancel",
+    ]);
+    const [changeStatus, setChangeStatus] = useState("");
+    const [orders, setOrders] = useState([]);
+    const [auth, setAuth] = useAuth();
+    const getOrders = async () => {
+        try {
+            const { data } = await axios.get("/api/v1/auth/all-orders");
+            setOrders(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        useEffect(() => {
+            if (auth?.token) getOrders();
+          }, [auth?.token]);
+        const handleChange = async (orderId, value) => {
+            try {
+                const { data } = await axios.put(`/api/v1/auth/order-status/${orderId}`, {
+                  status: value,
+                });
+                getOrders();
+              } catch (error) {
+                console.log(error);
+              }
+            };
+
+    return (
+        <Layout title={"All Orders AdminView"}>
+            <div className="row dashboard">
+                <div className="col-md-3">
+                < AdminMenu />
             </div>
-            <div className='col -md-9'>
-                <h1 className='col -md-9'>Order History</h1>
+            <div className='col-md-3'>
+                <h1 className='text-center'> All Orders </h1>
                 {orders?.map((o, i)=> {
                     return (
                       <div className='border shadow'>
@@ -46,7 +71,19 @@ const [auth, setAuth] = useAuth()
                           <tbody>
                             <tr>
                               <td>{i+1}</td>
-                              <td>{o?.status}</td>
+                              <td>
+                              <Select
+                                bordered={false}
+                                onChange={(value) => handleChange(o._id, value)}
+                                defaultValue={o?.status}
+                              >
+                                {status.map((s, i) => (
+                            <Option key={i} value={s}>
+                              {s}
+                            </Option>
+                          ))}
+                              </Select>
+                              </td>
                               <td>{o?.buyer?.name}</td>
                               <td>{moment(o?.createdAt).fromNow()}</td>
                               <td>{o?.payment.success? "Success":"Failed"}</td>
@@ -73,16 +110,14 @@ const [auth, setAuth] = useAuth()
                               </div>
                             </div>
                           ))}
-                        </div>
-                      </div>
-                    )
-                  })
-                }
-            </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-    </div>
-  </Layout>
-  )
-}
+      </div>
+    </Layout>
+  );
+};
 
-export default Orders
+export default AdminOrders;
